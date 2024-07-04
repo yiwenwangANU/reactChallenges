032c71b3996e67146f331cf564355f605c4728a4
admin01 <paradoxraphael@gmai.com>
2024-07-04 10:58:19 +1000

    use useEffect to update duration and adding sound with useCallback

--- Challenge_19_1/src/Calculator.js
+++ Challenge_19_1/src/Calculator.js
@@ -1,4 +1,4 @@
-import { useState, memo } from "react";
+import { useState, memo, useEffect, useCallback } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -6,17 +6,37 @@ function Calculator({ workouts, allowSound }) {
   const [sets, setSets] = useState(3);
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
+  const [duration, setDuration] = useState(0);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
-  const playSound = function () {
+  const playSound = useCallback(() => {
     if (!allowSound) return;
     const sound = new Audio(clickSound);
     sound.play();
-  };
+  }, [allowSound]);
 
+  useEffect(
+    function () {
+      setDuration(
+        (duration) => (number * sets * speed) / 60 + (sets - 1) * durationBreak
+      );
+      playSound();
+    },
+    [number, sets, speed, durationBreak, playSound]
+  );
+
+  function handleInc() {
+    setDuration((duration) => Math.floor(duration) + 1);
+    playSound();
+  }
+
+  function handleDec() {
+    setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
+    playSound();
+  }
   return (
     <>
       <form>
@@ -66,13 +86,13 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && "0"}
           {mins}:{seconds < 10 && "0"}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
